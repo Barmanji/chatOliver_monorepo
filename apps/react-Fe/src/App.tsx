b66a4719a051ee4ef41e9811ee6d4719ef96f52f
Ajay Barman <barmanjiaj@gmail.com>
2025-07-01 22:01:32 +0530

    refactor(app.tsx): modified and jumbled app.tsx

--- apps/react-Fe/src/App.tsx
+++ apps/react-Fe/src/App.tsx
@@ -1,11 +1,10 @@
 // import { useState } from 'react'
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router";
-import Page from "./components/Page";
-import Landing from "./components/Landing";
-import Login from "./components/Login";
-import Register from "./components/Register";
+import Landing from "./pages/Landing";
 import AuthLayout from "./components/AuthLayout";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
     // const [count, setCount] = useState(0)
@@ -15,7 +14,7 @@ function App() {
             <BrowserRouter>
                 <Routes>
                     <Route index element={<Landing />} />
-                    <Route path="page" element={<Page />} />
+                    {/* <Route path="page" element={<Page />} /> */}
                     <Route element={<AuthLayout />}>
                         <Route path="login" element={<Login />} />
                         <Route path="register" element={<Register />} />
