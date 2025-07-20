f379011e959cef45466a2d73d0ec4e5445766c97
Ajay Barman <barmanjiaj@gmail.com>
2025-07-20 18:18:21 +0530

    feat(state management): added redux

--- apps/react-Fe/src/pages/Landing.tsx
+++ apps/react-Fe/src/pages/Landing.tsx
@@ -1,8 +1,22 @@
+import MyComp from "../components/MyComp";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { increment, decrement } from "../store/slices/counter/counter";
 
 const Landing = () => {
-  return (
-    <div>Landing</div>
-  )
+  const count: any = useAppSelector((state) => state.counter);
+  const dispatch = useAppDispatch();
+
+return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Count is {count}</h1>
+        <button className="px-4" onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
+      </header>
+
+      <MyComp />
+    </div>
+  );
 }
 
 export default Landing
