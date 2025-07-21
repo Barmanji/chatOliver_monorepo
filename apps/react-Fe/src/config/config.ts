ce0a84e852351f800cc78eadc5455330de279193
Ajay Barman <barmanjiaj@gmail.com>
2025-07-21 23:26:25 +0530

    fix(API): bifurcated Server and baseURL

--- apps/react-Fe/src/config/config.ts
+++ apps/react-Fe/src/config/config.ts
@@ -1,4 +1,5 @@
 export const config = {
 
-    server: String(import.meta.env.VITE_BASE_URL)
+    server: String(import.meta.env.VITE_SERVER_URL),
+    baseUrl:String(import.meta.env.VITE_BASE_URL)
 }
