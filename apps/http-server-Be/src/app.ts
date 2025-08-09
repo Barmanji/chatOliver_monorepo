4cd823262ea4e4a8e8185dfa1930980f835d46b7
Ajay Barman <barmanjiaj@gmail.com>
2025-08-09 23:27:12 +0530

    fix(http-server-Be): fix cors

--- apps/http-server-Be/src/app.ts
+++ apps/http-server-Be/src/app.ts
@@ -1,32 +1,95 @@
-import express, {Express}  from "express";
+import dotenv from "dotenv";
+import express, { Express } from "express";
 import cors from "cors";
+import requestIp from "request-ip";
 import cookieParser from "cookie-parser";
+import { createServer } from "http";
+import { rateLimit } from "express-rate-limit";
+import { Server } from "socket.io";
+import session from "express-session";
+import passport from "passport";
+import { ApiError } from "./utils/ApiError.js";
+import morganMiddleware from "./logger/morgor.logger.js";
+import { initializeSocketIO } from "./socket/socket.js";
 
-const app: Express = express();
+dotenv.config({ path: "./.env" });
+
+
+const app = express();
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  pingTimeout: 60000,
+  cors: {
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  },
+});
+
+app.set("io", io); // using set method to mount the `io` instance on the app to avoid usage of `global`
+
+// global middlewares
 app.use(
-    cors({
-        origin: function (origin, callback) {
-            if (!origin) return callback(null, true);
-            if (/^http:\/\/localhost:\d+$/.test(origin)) {
-                return callback(null, true);
-            }
-            return callback(new Error("Not allowed by CORS"));
-        },
-        credentials: true,
-    })
+  cors({
+    origin:
+      process.env.CORS_ORIGIN === "*"
+        ? "*" // This might give CORS error for some origins due to credentials set to true
+        : process.env.CORS_ORIGIN?.split(","), // For multiple cors origin for production. Refer https://github.com/hiteshchoudhary/apihub/blob/a846abd7a0795054f48c7eb3e71f3af36478fa96/.env.sample#L12C1-L12C12
+    credentials: true,
+  })
 );
+
+app.use(requestIp.mw());
+
+// Rate limiter to avoid misuse of the service and avoid cost spikes
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5000, // Limit each IP to 500 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    //@ts-ignore
+  // keyGenerator: (req, res) => {
+  //   return req.clientIp; // IP address from requestIp.mw(), as opposed to req.ip
+  // },
+  handler: (_, __, ___, options) => {
+    throw new ApiError(
+      options.statusCode || 500,
+      `There are too many requests. You are only allowed ${
+        options.max
+      } requests per ${options.windowMs / 60000} minutes`
+    );
+  },
+});
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter);
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public"));
+app.use(express.static("public")); // configure static file to save images locally
 app.use(cookieParser());
+
+// required for passport
+app.use(
+  session({
+    secret: process.env.EXPRESS_SESSION_SECRET!,
+    resave: true,
+    saveUninitialized: true,
+  })
+); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+app.use(morganMiddleware);
+initializeSocketIO(io);
 
 // route imports
 import userRouter from "./routes/user.routes.js";
 import chatRouter from "./routes/chat.routes.js";
 import messageRouter from "./routes/message.routes.js";
-import groupRouter from "./routes/group.routes.js";
-import friendRequestRouter from  "./routes/friend-request.routes.js"
+
+import friendRequestRouter from "./routes/friend-request.routes.js";
 import notificationRouter from "./routes/notification.routes.js";
 import callLogRouter from "./routes/callLog.routes.js";
 import healthcheckRouter from "./routes/healthcheck.routes.js";
@@ -35,10 +98,9 @@ import healthcheckRouter from "./routes/healthcheck.routes.js";
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/chats", chatRouter);
 app.use("/api/v1/messages", messageRouter);
-app.use("/api/v1/groups", groupRouter);
 app.use("/api/v1/friend-requests", friendRequestRouter);
 app.use("/api/v1/notifications", notificationRouter);
 app.use("/api/v1/call-logs", callLogRouter);
 app.use("/api/v1/healthcheck", healthcheckRouter);
 
-export { app };
+export { httpServer };
